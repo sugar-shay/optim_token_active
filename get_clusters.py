ab9ab189678833c2cea26b7b0d8764bd89831718
sugar-shay <72109721+sugar-shay@users.noreply.github.com>
2021-09-17 15:29:31 -0500

    Add files via upload

---
+++ get_clusters.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 12:29:53 2021
+
+@author: Shadow
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import SpectralClustering
+
+
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+
+from collections import Counter
+import pandas as pd
+
+import pickle 
+import os
+from collections import Counter 
+
+def get_clusters(cluster_data):
+    
+    #clustering 2d does not include correctnesss as a feature
+    #this is how the clusters were created in datamaps and mind your outliers 
+    #the first two columns correspond to variance and confidence
+    cluster_train_data = cluster_data.iloc[:,0:2]
+    
+    print('Cluster train data shape: ', cluster_train_data.shape)
+    print('cluster train data head: ', cluster_train_data.head())
+    clustering_2d = SpectralClustering(n_clusters=3,
+                                       assign_labels='discretize',
+                                       random_state=0).fit(cluster_train_data)
+    
+    return {'cluster2d_labels':clustering_2d.labels_}
+
+def get_cluster_regions(cluster_labels, cluster_data_df, token_input_data):
+    
+    cluster_map = pd.DataFrame()
+    cluster_map['data_index'] = [i for i in range(0, len(cluster_labels))]
+    cluster_map['cluster'] = cluster_labels
+    
+     #Cluster 1
+    cluster1_mask = np.array(cluster_map[cluster_map.cluster == 0].data_index)
+    #print('cluster mask shape: ', cluster1_mask.shape)
+    cluster1 = cluster_data_df.iloc[cluster1_mask,:]
+    cluster1_avg_conf = np.mean(cluster1['confidence'].numpy())
+    cluster1_token_input = token_input_data.iloc[cluster1_mask, :]
+    
+    #Cluster 2
+    cluster2_mask = np.array(cluster_map[cluster_map.cluster == 1].data_index)
+    #print('cluster mask shape: ', cluster1_mask.shape)
+    cluster2 = cluster_data_df.iloc[cluster2_mask,:]
+    cluster2_avg_conf = np.mean(cluster2['confidence'].numpy())
+    cluster2_token_input = token_input_data.iloc[cluster2_mask, :]
+ 
+    #Cluster 3
+    cluster3_mask = np.array(cluster_map[cluster_map.cluster == 2].data_index)
+    #print('cluster mask shape: ', cluster1_mask.shape)
+    cluster3 = cluster_data_df.iloc[cluster3_mask,:]
+    cluster3_avg_conf = np.mean(cluster3['confidence'].numpy())
+    cluster3_token_input = token_input_data.iloc[cluster3_mask, :]
+
+    cluster_info = [(cluster1_avg_conf, cluster1_token_input, cluster1), 
+                    (cluster2_avg_conf, cluster2_token_input, cluster2), 
+                    (cluster3_avg_conf, cluster3_token_input, cluster3)]
+    
+    sorted_clusters = sorted(cluster_info, key=lambda x: x[0])
+    
+    cluster_regions = {}
+    cluster_regions['hard'] = sorted_clusters[0]
+    cluster_regions['ambig'] = sorted_clusters[1]
+    cluster_regions['easy'] = sorted_clusters[2]
+    
+    return cluster_regions
+
+def main(directory, sample_size=100000):
+    
+    
+    with open(directory+'/'+'bert_train_stats.pkl', 'rb') as f:
+        log = pickle.load(f)
+        
+    with open(directory+'/'+'bert_test_stats.pkl', 'rb') as f:
+        test_log = pickle.load(f)
+        
+    with open(directory+'/'+'token_inputs.pkl', 'rb') as f:
+        token_inputs = pickle.load(f) 
+        
+    #Calculating the variance, confidence, and correctness of the training samples
+    confidence = np.mean(log['gt_probs'], axis=-1)
+    variance = np.var(log['gt_probs'], axis=-1)
+    #correctness = np.mean(log['correctness'], axis = -1)
+    
+    correctness = []
+    for idx in range(log['correctness'].shape[0]):
+        
+        correct_bool = log['correctness'][idx,:]
+        correctness.append(np.count_nonzero(correct_bool))
+    
+    correctness = np.array(correctness)
+    
+    #Performing our cluster analysis
+    
+    
+    #this includes correctness
+    cluster_data_df = pd.DataFrame(data={'variance': variance, 
+                                         'confidence':confidence, 
+                                         'correctness':correctness, 
+                                         'labels': token_inputs['train_labels']})
+    
+    
+    
+    token_input_data = pd.DataFrame(data={'token_label_maks': [token_inputs['token_label_masks'][idx, :] for idx in range(token_inputs['token_label_masks'].shape[0])],
+                                         'labels': token_inputs['train_labels'],
+                                         'attention_mask': [token_inputs['attention_mask'][idx, :] for idx in range(token_inputs['attention_mask'].shape[0])],
+                                         'input_ids': [token_inputs['input_ids'][idx, :] for idx in range(token_inputs['input_ids'].shape[0])],
+                                         'sentence_idxs': token_inputs['sentence_idxs'],
+                                         'token_idxs':token_inputs['token_idxs'],
+                                         })
+    
+    cluster_sample_mask = np.random.RandomState(seed=21).permutation(cluster_data_df.shape[0])
+    cluster_sample_mask = cluster_sample_mask[0:sample_size]
+    
+    cluster_data_sample = cluster_data_df.iloc[cluster_sample_mask, :]
+    token_input_sample = token_input_data.iloc[cluster_sample_mask, :]
+    
+    clusters = get_clusters(cluster_data_sample)
+    with open(directory+'/'+'clusters.pkl', 'wb') as f:
+        pickle.dump(clusters, f)
+    
+    if not os.path.exists(directory+'/token_data'):
+        os.makedirs(directory+'/token_data')
+
+    cluster_regions = get_cluster_regions(clusters['cluster2d_labels'], cluster_data_sample, token_input_sample)
+    
+    tag_count = Counter(cluster_regions['easy'][2]['labels'].to_list())
+    print()
+    print('The # of "O" tags in Easy Cluster: ', tag_count['O'])
+    print('The # of "SN" tags in Easy Cluster: ', tag_count['SN'])
+    print('The # of "SV" tags in Easy Cluster: ', tag_count['SV'])
+    
+    tag_count = Counter(cluster_regions['ambig'][2]['labels'].to_list())
+    print()
+    print('The # of "O" tags in Ambigious Cluster: ', tag_count['O'])
+    print('The # of "SN" tags in Ambigious Cluster: ', tag_count['SN'])
+    print('The # of "SV" tags in Ambigious Cluster: ', tag_count['SV'])
+    
+    tag_count = Counter(cluster_regions['hard'][2]['labels'].to_list())
+    print()
+    print('The # of "O" tags in Hard Cluster: ', tag_count['O'])
+    print('The # of "SN" tags in Hard Cluster: ', tag_count['SN'])
+    print('The # of "SV" tags in Hard Cluster: ', tag_count['SV'])
+
+
+    with open(directory+'/token_data/sample_data.pkl', 'wb') as f:
+        pickle.dump(token_input_sample, f)
+    
+    with open(directory+'/token_data/easy_data.pkl', 'wb') as f:
+        pickle.dump(cluster_regions['easy'][1], f)
+    
+    with open(directory+'/token_data/ambig_data.pkl', 'wb') as f:
+        pickle.dump(cluster_regions['ambig'][1], f)
+    
+    with open(directory+'/token_data/hard_data.pkl', 'wb') as f:
+        pickle.dump(cluster_regions['hard'][1], f)
+        
+        
+
+
+if __name__ == "__main__":
+    
+    main(directory = 'results/memc', sample_size=5000)
+        
---
+++ tokenize_data.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 10:34:41 2021
+
+@author: Shadow
+"""
+
+from data_preprocess import *
+
+import numpy as np
+import torch
+
+
+from transformers import AutoTokenizer
+
+class NER_tokenizer():
+    
+    def __init__(self, unique_labels, max_length, tokenizer_name = None):
+        self.unique_tags = unique_labels
+        self.tag2id = {tag: id for id, tag in enumerate(self.unique_tags)}
+        self.id2tag = {id: tag for tag, id in self.tag2id.items()}
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True) if tokenizer_name is not None else AutoTokenizer.from_pretrained('bert-base-cased', use_fast=True)
+        self.max_len = max_length
+    
+    
+    def tokenize_and_encode_labels(self, data):
+        
+        '''
+        sequences = []
+        labels = []
+        for data_point in data:
+            seq = data_point[0]
+            seq = seq.split()
+            sequences.append(seq)
+            labels.append(data_point[1])
+        '''
+        encodings = self.tokenizer(data['text'].to_list(), is_split_into_words=True, return_offsets_mapping=True, max_length = self.max_len, padding=True, truncation=True)
+        
+        label_enc = self.encode_tags(data['labels'], encodings)
+        encodings.pop("offset_mapping") 
+        
+        #print('lablel enc shape: ', len(label_enc))
+        #print('Num slot shape: ', len(data['num_slots'].to_list()))
+        dataset = NER_Dataset(encodings, label_enc, data['num_slots'].to_list())
+        
+        return dataset
+    
+    def tokenize_unlabeled_data(self, data):
+        
+        sequences = []
+        for data_point in data:
+            seq = data_point[0]
+            seq = seq.split()
+            sequences.append(seq)
+        
+        encodings = self.tokenizer(sequences, is_split_into_words=True, max_length = self.max_len, padding=True, truncation=True)
+
+        return encodings
+        
+        
+    def encode_tags(self, tags, encodings):
+        
+        labels = [[self.tag2id[tag] for tag in doc] for doc in tags]
+        encoded_labels = []
+        for doc_labels, doc_offset in zip(labels, encodings.offset_mapping):
+            #print('len doc labels: ', len(doc_labels))
+            # create an empty array of -100
+            doc_enc_labels = np.ones(len(doc_offset),dtype=int) * -100
+            #print('doc enc labels shape: ', len(doc_enc_labels))
+            arr_offset = np.array(doc_offset)
+            #print('arr offset shaoe: ', arr_offset.shape)
+        
+            # set labels whose first offset position is 0 and the second is not 0
+            #doc_enc_labels[(arr_offset[:,0] == 0) & (arr_offset[:,1] != 0)] = doc_labels
+            counter = 0
+            for i in range(arr_offset.shape[0]):
+                if (arr_offset[i,0] == 0) and (arr_offset[i,1] != 0):
+                    doc_enc_labels[i] = doc_labels[counter]
+                    counter += 1
+            #print('counter: ', counter)
+            
+            encoded_labels.append(doc_enc_labels.tolist())
+        
+        return encoded_labels
+
+  
+class NER_Dataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels, num_slots):
+        self.encodings = encodings
+        self.labels = labels
+        self.num_slots = num_slots
+        self.sentence_idxs = np.array([x for x in range(0, len(self.encodings['input_ids']))])
+
+        
+    
+    def __getitem__(self, idx):
+        item = {key: torch.LongTensor(val[idx]) for key, val in self.encodings.items()}
+        #item['num_slots'] = torch.LongTensor(self.num_slots[idx])
+        
+        item['sentence_idxs'] = torch.tensor(self.sentence_idxs[idx])
+        if self.labels is not None:
+            item['labels'] = torch.LongTensor(self.labels[idx])
+            
+        if self.num_slots is not None:
+            item['num_slots'] = torch.tensor(self.num_slots[idx])
+            
+        #print('Encoding Shape: ', item['input_ids'].shape)
+        #print('Labels Shape: ', item['labels'].shape)
+        #print('Num Slot Shape: ', item['num_slots'].shape)
+        return item
+    
+    def __len__(self):
+        return len(self.encodings['input_ids'])
+    
+class Token_Level_Dataset(torch.utils.data.Dataset):
+    def __init__(self, input_ids, attention_mask, token_idxs, token_label_masks, labels=None):
+        self.input_ids = torch.tensor(input_ids)
+        self.attention_mask = torch.tensor(attention_mask)
+        self.token_idxs = token_idxs
+        self.token_label_masks = torch.tensor(token_label_masks)
+        self.labels = labels
+        
+        print('labels shape: ', self.labels.shape)
+        print('token idxs shape: ', self.token_idxs.shape)
+        
+    def __getitem__(self, idx):
+        item = {}
+        item['input_ids'] = self.input_ids[idx, :]
+        item['attention_mask'] = self.attention_mask[idx, :]
+        
+        #we have to apply padding to make sure token idxs and labels are all the same shape
+        item['token_idxs'] = torch.IntTensor(self.token_idxs[idx, :])
+        #item['token_idxs'] = torch.nn.functional.pad(torch.tensor(self.token_idxs[idx]), pad =(64,1), value=-100)
+        item['token_label_masks'] = self.token_label_masks[idx,:]
+        if self.labels is not None:
+            item['token_labels'] = torch.IntTensor(self.labels[idx, :])
+            #item['token_labels'] = torch.nn.functional.pad(torch.tensor(self.labels[idx]), pad=(64,1), value=-100)
+        return item
+
+    def __len__(self):
+        return self.input_ids.shape[0]
+        
+
+
